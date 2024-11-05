ef4ca25252f39a971756366486c0ccbbe0918659
HARSH <harshjangid33815@gmail.com>
2024-11-06 00:15:42 +0530

    Add files via upload

---
+++ about1.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- font awesome cdn link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    
+     <link rel="stylesheet" href="styles.css">
+
+
+</head>
+<body>
+    
+<!-- header section-->
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about1.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends-->
+    <div class="heading" style= "background:url(images/download (1).jfif)  no repeat">
+        <h1>about us</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--footer section-->
+<section class="footer">
+
+<div class="box-container">
+   <div class="box">
+      <h3>quick links</h3>
+      <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+      <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+      <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+      <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+   </div>
+
+   <div class="box">
+      <h3>extra links</h3>
+      <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+      <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+      <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+      <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+   </div>
+
+   <div class="box">
+      <h3>contact info</h3>
+      <a href="#"> <i class="fas fa-phone"></i> +91-9050843054</a>
+      <a href="#"> <i class="fas fa-phone"></i> +91-9050363054</a>
+      <a href="#"> <i class="fas fa-envelope"></i> harshjangid33815@gmail.com</a>
+      <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104</a>
+   </div>
+
+   <div class="box">
+      <h3>follow us</h3>
+      <a href="#"> <i class="fas fa-instagram"></i>instagram</a>
+      <a href="#"> <i class="fas fa-twitter"></i>twitter</a>
+      <a href="#"> <i class="fas fa-linkedin"></i>linkedin</a>
+
+</div>
+
+<div class="credit"> created by <span>Mr. Harsh</span> | all right reserved!</div>
+</section>
+<!-- javascript file link  -->
+ <script src="script.js"></script>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- font awesome cdn link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    
+     <link rel="stylesheet" href="styles.css">
+
+
+</head>
+<body>
+    
+<!-- header section-->
+ <section class="header">
+
+ <a href="home.php" class="logo">travel.</a>
+
+ <nav class="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">package</a>
+    <a href="book.php">book</a>
+ </nav>
+ </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- javascript file link  -->
+ <script src="script.js"></script>
+
+</body>
+</html>
